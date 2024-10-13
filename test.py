b410721ee8725cc6665935a1c5554c5ef5c473ec
Milan Svitlica <svitlicamilan@gmail.com>
2024-10-13 22:36:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -11,4 +11,18 @@ hello world {2}
 hello world {3}
 """)
 
+age = 20
+name = "John"
+
+print(f"hello world {age} {name}")
+
+
+
+height = 1.75
+weight = 70
+
+bmi = weight / height ** 2
+
+print(f"hello world {bmi:.4f}")
+
 
