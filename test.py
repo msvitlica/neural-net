edfdecfd2b5e6f97e56d021e637ee94c63fa4977
Milan Svitlica <svitlicamilan@gmail.com>
2024-10-13 22:29:23 +0200

    examples

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
 print("hello world 1")
  
+ # i want to print formatted string
+
+print(f"hello world {1}")
+
+# ok now I want mutiline string example, with formatted floating point numbers in every line
+
+print(f"""hello world {1}
+hello world {2}
+hello world {3}
+""")
+
+
